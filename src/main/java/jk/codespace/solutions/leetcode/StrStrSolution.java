c6d72f9d2416ea8676bf17452dc5c098c137ae06
John Kelly <kellyjra@gmail.com>
2021-01-03 15:53:06 +0000

    chore: adding leetcode problem link to comments

--- src/main/java/jk/codespace/solutions/leetcode/StrStrSolution.java
+++ src/main/java/jk/codespace/solutions/leetcode/StrStrSolution.java
@@ -1,6 +1,9 @@
 package jk.codespace.solutions.leetcode;
 
 /*
+Solution for leetcode problem;
+https://leetcode.com/problems/implement-strstr/
+
 Implement strStr().
 
 Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
